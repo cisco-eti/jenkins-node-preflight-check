2df3d1badde28d434de9cf7ff1f8dd4f0453b1b2
Ashish Arora <ashisar2@cisco.com>
2020-05-11 12:32:59 -0500

    docker: extract only binary in final image

--- build/build-docker.sh
+++ build/build-docker.sh
@@ -1,11 +1,32 @@
-#!/bin/bash
+#!/bin/bash -e
 # Syntax build-docker.sh [-i|--image imagename]
 
 PROJECT=go-template
 DOCKER_IMAGE=nyota-gotemplate
+BASE_DOCKER_IMAGE=nyota-gotemplate-base
 H_OUT=index.html
 S_OUT=staticanalysis.txt
 
+static_analysis(){
+     # extract the S_OUT file from the docker image just created
+    id=$(docker create ${BASE_DOCKER_IMAGE})
+    docker cp ${id}:/go/src/sqbu-github.cisco.com/Nyota/${PROJECT}/${S_OUT} .
+    docker rm -v ${id}
+
+    if [[ ! -d "pipeline/lib" ]] ; then
+        echo "Your static analysis report is in $S_OUT"
+    fi
+}
+
+code_coverage() {
+    # extract the H_OUT file from the docker image just created
+    id=$(docker create ${BASE_DOCKER_IMAGE})
+    docker cp ${id}:/go/src/sqbu-github.cisco.com/Nyota/${PROJECT}/${H_OUT} .
+    docker rm -v ${id}
+    if [[ ! -d "pipeline/lib" ]] ; then
+        echo "Your coverage HTML report is in $H_OUT"
+    fi
+}
 
 while [[ $# -gt 0 ]]
 do
@@ -34,26 +55,28 @@ do
     esac
 done
 
-echo BUILDING DOCKER $DOCKER_IMAGE
-docker build -t $DOCKER_IMAGE -f build/Dockerfile --build-arg HTML_OUT=$H_OUT --build-arg CODE_COVERAGE=$CODE_COVERAGE --build-arg STATIC_ANALYSIS=$STATIC_ANALYSIS --build-arg SA_OUT=$S_OUT .
+echo BUILDING DOCKER ${BASE_DOCKER_IMAGE}
+docker build -t \
+        ${BASE_DOCKER_IMAGE} -f build/Dockerfile \
+        --build-arg HTML_OUT=${H_OUT} \
+        --build-arg CODE_COVERAGE=${CODE_COVERAGE} \
+        --build-arg STATIC_ANALYSIS=${STATIC_ANALYSIS} \
+         --build-arg SA_OUT=${S_OUT} \
+         .
 
-if [ "${CODE_COVERAGE}" = "cc" ] ; then
-    # extract the H_OUT file from the docker image just created
-    id=$(docker create $DOCKER_IMAGE)
-    docker cp $id:/go/src/sqbu-github.cisco.com/Nyota/${PROJECT}/$H_OUT .
-    docker rm -v $id
-    if [ ! -d "pipeline/lib" ] ; then
-        echo "Your coverage HTML report is in $H_OUT"
-    fi
+if [[ "${CODE_COVERAGE}" = "cc" ]] ; then
+    echo "Generating code coverage"
+    code_coverage
 fi
 
-if [ "${STATIC_ANALYSIS}" = "sa" ] ; then
-    # extract the S_OUT file from the docker image just created
-    id=$(docker create $DOCKER_IMAGE)
-    docker cp $id:/go/src/sqbu-github.cisco.com/Nyota/${PROJECT}/$S_OUT .
-    docker rm -v $id
-
-    if [ ! -d "pipeline/lib" ] ; then
-        echo "Your static analysis report is in $S_OUT"
-    fi
+if [[ "${STATIC_ANALYSIS}" = "sa" ]] ; then
+    echo "Generating static analysis"
+   static_analysis
 fi
+
+echo BUILDING DOCKER ${DOCKER_IMAGE}
+
+docker build --no-cache -t ${DOCKER_IMAGE} -f build/Imagefile .
+
+# remove base image and its subordinate images
+docker image rmi ${BASE_DOCKER_IMAGE} || true
