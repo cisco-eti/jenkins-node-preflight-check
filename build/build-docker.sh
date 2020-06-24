fd8db9addc4d1e6dc0f0f198437602024ad322cc
Saketh Ram Potturi <sakpottu@cisco.com>
2020-06-24 04:27:32 -0500

    build: parse artifactory credentials

--- build/build-docker.sh
+++ build/build-docker.sh
@@ -2,20 +2,44 @@
 # Syntax build-docker.sh [-i|--image imagename]
 
 PROJECT=go-template
-DOCKER_IMAGE=nyota-gotemplate
-BASE_DOCKER_IMAGE=nyota-gotemplate-base
+DOCKER_IMAGE=nyota-${PROJECT}:latest
+BASE_DOCKER_IMAGE=nyota-${PROJECT}:base
 H_OUT=index.html
 S_OUT=staticanalysis.txt
 
-static_analysis(){
-     # extract the S_OUT file from the docker image just created
-    id=$(docker create ${BASE_DOCKER_IMAGE})
-    docker cp ${id}:/go/src/sqbu-github.cisco.com/Nyota/${PROJECT}/${S_OUT} .
-    docker rm -v ${id}
+get_artifactory_credentials() {
+  if [[ "${ARTIFACTORY_USER}" != "" ]] && [[ "${ARTIFACTORY_PASSWORD}" != "" ]]; then
+    return
+  fi
 
-    if [[ ! -d "pipeline/lib" ]] ; then
-        echo "Your static analysis report is in $S_OUT"
+  if [[ "${NYOTA_CREDENTIALS_FILE}" = "" ]]; then
+    NYOTA_CREDENTIALS_FILE=~/.nyota/credentials
+    echo "Using DEFAULT Artifactory credentials file: $NYOTA_CREDENTIALS_FILE"
+  else
+    echo "Using Artifactory credentials file: $NYOTA_CREDENTIALS_FILE"
+  fi
+
+  [[ "$(uname)" = Darwin ]] && NYOTA_CREDENTIALS_FILE_MOD=$(stat -f "%p" "${NYOTA_CREDENTIALS_FILE}" | cut -c4-) || NYOTA_CREDENTIALS_FILE_MOD=$(stat -c "%a" "${NYOTA_CREDENTIALS_FILE}")
+  if [[ ${NYOTA_CREDENTIALS_FILE_MOD} != "400" ]]; then
+    echo "File ${NYOTA_CREDENTIALS_FILE} must have 400 mod permission"
+    exit 1
+  fi
+
+  if [[ "$NYOTA_CREDENTIALS_SECTION" == "" ]]; then
+    NYOTA_CREDENTIALS_SECTION=default
+  fi
+
+  while IFS=' = ' read key value; do
+    if [[ ${key} == \[*] ]]; then
+      section=${key}
+    elif [[ ${value} ]] && [[ ${section} == "[${NYOTA_CREDENTIALS_SECTION}]" ]]; then
+      if [[ ${key} == 'artifactory_user' ]]; then
+        ARTIFACTORY_USER=${value}
+      elif [[ ${key} == 'artifactory_password' ]]; then
+        ARTIFACTORY_PASSWORD=${value}
+      fi
     fi
+  done <${NYOTA_CREDENTIALS_FILE}
 }
 
 code_coverage() {
@@ -25,6 +49,17 @@ code_coverage() {
     docker rm -v ${id}
     if [[ ! -d "pipeline/lib" ]] ; then
         echo "Your coverage HTML report is in $H_OUT"
+    fi
+}
+
+static_analysis() {
+    # extract the S_OUT file from the docker image just created
+    id=$(docker create ${BASE_DOCKER_IMAGE})
+    docker cp ${id}:/go/src/sqbu-github.cisco.com/Nyota/${PROJECT}/${S_OUT} .
+    docker rm -v ${id}
+
+    if [[ ! -d "pipeline/lib" ]] ; then
+        echo "Your static analysis report is in $S_OUT"
     fi
 }
 
@@ -55,14 +90,18 @@ do
     esac
 done
 
+get_artifactory_credentials
 echo BUILDING DOCKER ${BASE_DOCKER_IMAGE}
-docker build -t \
-        ${BASE_DOCKER_IMAGE} -f build/Dockerfile \
-        --build-arg HTML_OUT=${H_OUT} \
-        --build-arg CODE_COVERAGE=${CODE_COVERAGE} \
-        --build-arg STATIC_ANALYSIS=${STATIC_ANALYSIS} \
-         --build-arg SA_OUT=${S_OUT} \
-         .
+
+docker pull containers.cisco.com/nyota/frontline-base-golang-docker:latest
+docker build --no-cache \
+    -t ${BASE_DOCKER_IMAGE} \
+    -f build/Dockerfile \
+    --build-arg HTML_OUT=${H_OUT} \
+    --build-arg CODE_COVERAGE=${CODE_COVERAGE} \
+    --build-arg STATIC_ANALYSIS=${STATIC_ANALYSIS} \
+    --build-arg SA_OUT=${S_OUT} \
+    .
 
 if [[ "${CODE_COVERAGE}" = "cc" ]] ; then
     echo "Generating code coverage"
@@ -71,7 +110,7 @@ fi
 
 if [[ "${STATIC_ANALYSIS}" = "sa" ]] ; then
     echo "Generating static analysis"
-   static_analysis
+    static_analysis
 fi
 
 echo BUILDING DOCKER ${DOCKER_IMAGE}
