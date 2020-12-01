2eb069d6c871a7a2c093e8c16b15da26aad81d9a
Sri Aradhyula <sraradhy@cisco.com>
2020-12-01 15:56:33 -0600

    fix routes_test.go add v1 back

--- pkg/v1/routes_test.go
+++ pkg/v1/routes_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestRoutes_GetDeviceZones(t *testing.T) {
 	t.Run("returns Device_A zone", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/device/A", nil)
+		request, _ := http.NewRequest(http.MethodGet, "/v1/device/A", nil)
 		response := httptest.NewRecorder()
 		request.Header.Set("Authorization", "Bearer 123456")
 		router := mux.NewRouter()
@@ -26,7 +26,7 @@ func TestRoutes_GetDeviceZones(t *testing.T) {
 	})
 
 	t.Run("returns Device_B zone", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/device/B", nil)
+		request, _ := http.NewRequest(http.MethodGet, "/v1/device/B", nil)
 		response := httptest.NewRecorder()
 		request.Header.Set("Authorization", "Bearer 123456")
 		router := mux.NewRouter()
