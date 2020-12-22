0f1e3b748aab68bf0781a9437656cb92a42eb803
Sam Olds <saolds@cisco.com>
2020-12-22 16:11:37 -0700

    initializes dependencies up front in main
    the logger will be initialized and a connection to the db will be
    opened once, up front, in main. then a reference to the logger and
    db is passed through to all of the handlers. this provides a handful
    of benefits:
    - the handlers don't have to be concerned with dependency initialization
      (separation of concerns)
    - any errors during initialization of these dependencies will happen on
      boot, instead of potentially during request handling (fail loud, fail
      fast)
    
    this PR also cleans up some error handling throughout the various
    utilities and handlers

--- pkg/middleware/oauth_middleware.go
+++ pkg/middleware/oauth_middleware.go
@@ -2,35 +2,23 @@ package middleware
 
 import (
 	"net/http"
-	"os"
 
-	log "github.com/sirupsen/logrus"
+	etilogger "wwwin-github.cisco.com/eti/sre-go-logger"
 )
 
-func init() {
-	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
-}
-
 // OAuthMiddleware adds Mux middleware operations to authenticate requests
-func OAuthMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//res := utils.HTTPResponse{ResponseWriter: w}
-		//token := r.Header.Get("Authorization")
-		//log.Info(r.RequestURI + token)
-		//if token != "Bearer 123456" && r.RequestURI != "/metrics" {
-		//	res.UnauthorizedResponse()
-		//	return
-		//}
-		log.Info("Authorized request for " + r.RequestURI)
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
-		next.ServeHTTP(w, r)
-	})
+func OAuthMiddleware(log *etilogger.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//token := r.Header.Get("Authorization")
+			//log.Info(r.RequestURI + token)
+			//if token != "Bearer 123456" {
+			//	utils.UnauthorizedResponse(w)
+			//	return
+			//}
+
+			log.Info("Authorized request for " + r.RequestURI)
+			next.ServeHTTP(w, r)
+		})
+	}
 }
