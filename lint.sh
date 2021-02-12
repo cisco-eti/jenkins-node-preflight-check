926ad4a2a4a8aacfa9be8103531484d521aae7b0
Sri Aradhyula <sraradhy@cisco.com>
2021-02-11 23:26:46 -0600

    fix: rename artifactory_user->ARTIFACTORY_USER

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 export GO111MODULE=on
 export GOPRIVATE="wwwin-github.cisco.com"
 export GONOPROXY="github.com,gopkg.in,go.uber.org"
-export GOPROXY=https://${artifactory_user}:${artifactory_password}@engci-maven-master.cisco.com/artifactory/api/go/nyota-go
+export GOPROXY=https://${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}@engci-maven-master.cisco.com/artifactory/api/go/nyota-go
 
 go get ./...
 golangci-lint run
