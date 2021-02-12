ae5f21c58d281872370e8d551d1e2ec58b7c7fe5
Sri Aradhyula <sraradhy@cisco.com>
2021-02-11 23:43:31 -0600

    fix: golangci-lint verbose

--- lint.sh
+++ lint.sh
@@ -4,4 +4,4 @@ export GONOPROXY="github.com,gopkg.in,go.uber.org"
 export GOPROXY=https://${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}@engci-maven-master.cisco.com/artifactory/api/go/nyota-go
 
 go get ./...
-golangci-lint run
+golangci-lint run -v
